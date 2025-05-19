71ed2d99785a6b57542ca3f04f4cfed7f80983e2
VAMSI <vamsi.vamsi21@gmail.com>
2025-05-19 15:05:07 +0530

    swap added

---
+++ swap.py
@@ -0,0 +1,32 @@
+a = 23
+b = 45
+
+print(f"a = {a}")
+print(f"b = {b}")
+
+
+
+
+'''b = a + b 
+a = b - a
+b = b - a'''
+
+b= a*b
+
+a = b/a
+
+b = b/a
+
+
+
+
+
+
+print("after swapping")
+
+print(f"a = {a}")
+print(f"b = {b}")
+
+
+
+
